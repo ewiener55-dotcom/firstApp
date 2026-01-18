4eddac94cfeb614dce5d8a0bff85c84d9731abd4
ewiener55-dotcom <ewiener55@gmail.com>
2026-01-18 16:50:28 -0500

    Delete backend directory

--- backend/backendApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ViewRateActivity,ViewRateActivityCRUD
-
-urlpatterns = [
-    path('activities/',ViewRateActivity.as_view()),
-    path('activities/<int:pk>/',ViewRateActivityCRUD.as_view())
-]
-
